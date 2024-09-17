71eb6705519b723577094c6f80f0a796ded66189
Harsh Vardhan <devharsh2k4@gmail.com>
2024-09-18 01:38:49 +0530

    added desktop nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { CampaignDetails,CreateCampaign,Home,Profile } from "./pages";
+import { Navbar, Sidebar } from "./components";
 
 const App = () => {
   return (
-	<div>App</div>
-  )
-}
+    <div className="relative sm:-8 p-4 bg-slate-950 min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative"><Sidebar/></div>
+	  <div className="flex-1 max-sm:w-full max-w-7xl mx-auto sm:pr-5"> <Navbar/> </div>
+	  <Routes>
+		<Route path="/" element={<Home/>}/>
+	  </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import  App  from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   
